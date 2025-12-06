e56ae5881272bf0b1e68d1291ef04c2000f83cf6
ReyyArchadiaa <ryyxiaoyan@gmail.com>
2025-12-07 05:37:09 +0700

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCCqpQ-CjewcY8kEj0Fl9f1wOFvcHMPL_0",
+    authDomain: "ryychatroom.firebaseapp.com",
+    projectId: "ryychatroom",
+    storageBucket: "ryychatroom.appspot.com",
+    messagingSenderId: "402217827316",
+    appId: "1:402217827316:web:fe5ae216516c56a90e17a1",
+    measurementId: "G-QESQED4GY3"
+};
